86bee0d1d1857a1cd2c9b27fae7e097a94750061
d9nich <dan.galaiko@gmail.com>
2020-06-24 16:25:23 +0300

    Lesson14 exercise5. A simple template class that holds two arrays of types that are defined via the class’s template parameter list. The size of the array is 10.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,78 @@
 
 using namespace std;
 
-template<typename T>
-void exchange(T &a, T &b);
+template<typename T1, typename T2>
+class DuoArray {
+    const static int SIZE_OF_ARRAY = 10;
+    T1 *firstArray;
+    T2 *secondArray;
+public:
+    DuoArray() {
+        firstArray = new T1[SIZE_OF_ARRAY];
+        secondArray = new T2[SIZE_OF_ARRAY];
+    }
+
+    DuoArray(const DuoArray<T1, T2> &srcArray) {
+        firstArray = new T1[SIZE_OF_ARRAY];
+        secondArray = new T2[SIZE_OF_ARRAY];
+        for (int i = 0; i < SIZE_OF_ARRAY; ++i) {
+            firstArray[i] = srcArray.firstArray[i];
+            secondArray[i] = srcArray.secondArray[i];
+        }
+    }
+
+    DuoArray &operator=(const DuoArray<T1, T2> &srcArray) {
+        if (this != srcArray && srcArray.firstArray != nullptr)
+            for (int i = 0; i < SIZE_OF_ARRAY; ++i) {
+                firstArray[i] = srcArray.firstArray[i];
+                secondArray[i] = srcArray.secondArray[i];
+            }
+        return *this;
+    }
+
+    DuoArray &operator=(DuoArray<T1, T2> &&srcArray) noexcept {
+        if (firstArray != nullptr && this != srcArray) {
+            delete[] firstArray;
+            delete[] secondArray;
+            firstArray = srcArray.firstArray;
+            srcArray.firstArray = nullptr;
+            secondArray = srcArray.secondArray;
+            srcArray.secondArray = nullptr;
+        }
+        return *this;
+    }
+
+    DuoArray(DuoArray<T1, T2> &&srcArray) noexcept {
+        if (srcArray.firstArray != nullptr) {
+            firstArray = srcArray.firstArray;
+            srcArray.firstArray = nullptr;
+            secondArray = srcArray.secondArray;
+            srcArray.secondArray = nullptr;
+        }
+    }
+
+
+    T1 *getFirstArray() const {
+        return firstArray;
+    }
+
+    T2 *getSecondArray() const {
+        return secondArray;
+    }
+
+    virtual ~DuoArray() {
+        delete[] firstArray;
+        delete[] secondArray;
+    }
+};
 
 int main() {
-    int a = 5;
-    int b = 7;
+    auto duoArray = new DuoArray<int, string>;
 
-    cout << "a = " << a << endl << "b = " << b << endl;
-    exchange(a, b);
+    duoArray->getFirstArray()[1] = 5;
+    duoArray->getSecondArray()[2] = "Bob";
 
-    cout << "a = " << a << endl << "b = " << b << endl;
-    return 0;
-}
+    cout << "Point of " << duoArray->getSecondArray()[2] << " -> " << duoArray->getFirstArray()[1];
 
-template<typename T>
-void exchange(T &a, T &b) {
-    T temp = a;
-    a = b;
-    b = temp;
+    delete duoArray;
 }
