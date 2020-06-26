ae0850601e51e5eaf07b3f7d141b6c32b9d5802d
d9nich <dan.galaiko@gmail.com>
2020-06-26 16:35:53 +0300

    Lesson 16 exercise 4 has been done. Program have four string objects that are initialized to “I,” “Love,” “STL,” and “String.” Appends them with a space in between and displays the sentence.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
 
 int main() {
-    cout << "Enter a sentence: ";
-    string userSentence;
-    getline(cin, userSentence);
-
-    transform(userSentence.begin(), userSentence.end(), userSentence.begin(), ::toupper);
-
-
-    cout << "Converted every alternate character of a string into uppercase: " << endl;
-    cout << userSentence << endl;
-
+    string first = "I";
+    string second = "Love";
+    string third = "STL";
+    string fourth = "String";
+    string result = first + " " + second + " " + third + " " + fourth;
+    cout << result;
     return 0;
 }
