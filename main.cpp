e68a9313af1e82e323b1a4c6ffed88241b7bb98d
d9nich <dan.galaiko@gmail.com>
2020-06-27 19:56:36 +0300

    Exercise 17.2 has been done. Extends the program from Exercise 1 to be able to tell the user whether a value he queries for already exists in the vector .

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-
 int main() {
     vector<int> integers;
-    while (true){
-        cout<<"Enter a number: ";
+    while (true) {
+        cout << "Enter a number: ";
         int number;
         cin >> number;
-        integers.push_back(number);
-        char answer;
-        cout<<"Do you want to watch element?(y, n): ";
-        cin >> answer;
-        if (answer=='y'){
-            cout<<"Enter number of index you want to watch: ";
-            int index=0;
-            cin>>index;
-            if (index>=0 && index<integers.size()){
-                cout<<"integers["<<index<<"] = "<<integers[index]<<endl;
-            } else
-                cout<<"Invalid index"<<endl;
+        char answer = 'y';
+        auto contains = find(integers.begin(), integers.end(), number);
+        if (contains!=integers.end()) {
+            cout << "Number is already in array, do you want to add it one more time?(y, n): ";
+            cin >> answer;
         }
-        cout<<"Do you want to continue?(y, n): ";
+        if (answer == 'y')
+            integers.push_back(number);
+
+        cout << "Do you want to watch element?(y, n): ";
         cin >> answer;
-        if (answer!='y')
+        if (answer == 'y') {
+            cout << "Enter number of index you want to watch: ";
+            int index = 0;
+            cin >> index;
+            if (index >= 0 && index < integers.size()) {
+                cout << "integers[" << index << "] = " << integers[index] << endl;
+            } else
+                cout << "Invalid index" << endl;
+        }
+        cout << "Do you want to continue?(y, n): ";
+        cin >> answer;
+        if (answer != 'y')
             break;
     }
     return 0;
