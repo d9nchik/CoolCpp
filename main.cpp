40d38cf79486acaa6e02fa9ff53d6e96b6c122aa
d9nich <dan.galaiko@gmail.com>
2020-06-28 11:57:12 +0300

    Exercise 18.2 has been done. A short program, demonstrate that an iterator pointing to an element in a list continues to remain valid even after another element has been inserted before it, thus changing the relative position of the former element.

--- main.cpp
+++ main.cpp
@@ -3,47 +3,12 @@
 
 using namespace std;
 
-template<typename T>
-void show(const T &elements);
-
-int DisplayOptions() {
-    cout << "What would you like to do?" << endl;
-    cout << "Select 1: To enter an integer" << endl;
-    cout << "Select 2: To display the vector" << endl << "> ";
-    cout << "Select 3: To quit!" << endl << "> ";
-    int ch;
-    cin >> ch;
-    return ch;
-}
-
 int main() {
-    list<int> elements;
-    int chUserChoice;
-    while ((chUserChoice = DisplayOptions()) != 3) {
-        switch (chUserChoice) {
-            case 1: {
-                cout << "Please enter an integer to be inserted: ";
-                int nDataInput = 0;
-                cin >> nDataInput;
-                elements.push_front(nDataInput);
-                break;
-            }
-            case 2:
-                cout << "The contents of the vector are: ";
-                show(elements);
-                cout << endl;
-                break;
-            default:
-                cout << "Incorrect choice" << endl;
-        }
-    }
-    return 0;
-}
+    list<int> ints = {10, 20};
+    auto first = ints.cbegin();
+    auto second = --ints.cend();
+    ints.insert(++ints.cbegin(), 15);
+    cout << "First: " << *first << "; Second: " << *second << endl;
 
-template<typename T>
-void show(const T &elements) {
-    cout << "[";
-    for (auto iterator = elements.cbegin(); iterator != elements.cend(); ++iterator)
-        cout << *iterator << " ";
-    cout << "]" << endl;
+    return 0;
 }
