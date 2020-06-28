939a7ae444e4e138b0d578ecb361cad818ffdcbc
d9nich <dan.galaiko@gmail.com>
2020-06-28 11:46:11 +0300

    Exercise 18.1 has been done. A short program that accepts numbers from the user and inserts them at the top of the list .

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
-#include <deque>
+#include <list>
 
 using namespace std;
 
 template<typename T>
-void show(deque<T> elements);
+void show(const T &elements);
+
+int DisplayOptions() {
+    cout << "What would you like to do?" << endl;
+    cout << "Select 1: To enter an integer" << endl;
+    cout << "Select 2: To display the vector" << endl << "> ";
+    cout << "Select 3: To quit!" << endl << "> ";
+    int ch;
+    cin >> ch;
+    return ch;
+}
 
 int main() {
-    deque<string> stringDeque = {"Hello"s, "Containers are cool!"s, "C++ is evolving"s};
-    show(stringDeque);
+    list<int> elements;
+    int chUserChoice;
+    while ((chUserChoice = DisplayOptions()) != 3) {
+        switch (chUserChoice) {
+            case 1: {
+                cout << "Please enter an integer to be inserted: ";
+                int nDataInput = 0;
+                cin >> nDataInput;
+                elements.push_front(nDataInput);
+                break;
+            }
+            case 2:
+                cout << "The contents of the vector are: ";
+                show(elements);
+                cout << endl;
+                break;
+            default:
+                cout << "Incorrect choice" << endl;
+        }
+    }
     return 0;
 }
 
 template<typename T>
-void show(deque<T> elements) {
-    for (int i = 0; i < elements.size(); ++i)
-        cout << elements[i] << endl;
+void show(const T &elements) {
+    cout << "[";
+    for (auto iterator = elements.cbegin(); iterator != elements.cend(); ++iterator)
+        cout << *iterator << " ";
+    cout << "]" << endl;
 }
