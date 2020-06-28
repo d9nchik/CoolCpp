31a91607425f1ccc9fc22dbf1d96d34de6fcdb1b
d9nich <dan.galaiko@gmail.com>
2020-06-28 12:03:50 +0300

    Exercise 18.2 has been done. A program that inserts the contents of a vector into an STL list using the  list’s insert() function.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <list>
+#include <vector>
 
 using namespace std;
 
+template<typename T>
+void show(const T &list);
+
 int main() {
-    list<int> ints = {10, 20};
-    auto first = ints.cbegin();
-    auto second = --ints.cend();
-    ints.insert(++ints.cbegin(), 15);
-    cout << "First: " << *first << "; Second: " << *second << endl;
+    vector<int> numbers = {23, 76, 12, 34, 1, 0};
+    list<int> lisNumbers = {25, 40};
+    lisNumbers.insert(lisNumbers.begin(), numbers.cbegin(), numbers.cend());
+    show(lisNumbers);
 
     return 0;
+}
+
+template<typename T>
+void show(const T &list) {
+    for (auto iterator = list.cbegin(); iterator != list.cend(); ++iterator)
+        cout << *iterator << " ";
+    cout << endl;
 }
