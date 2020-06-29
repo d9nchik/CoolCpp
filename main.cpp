d8b9c3083e430f2356294ce4f62b5da5c9eb1541
d9nich <dan.galaiko@gmail.com>
2020-06-29 16:50:35 +0300

    Exercise 19.1 has been done. Program imitate telephone dictionary and find a person’s name given a phone number.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,61 @@
+#include <set>
 #include <iostream>
-#include <list>
-#include <vector>
+#include <string>
 
 using namespace std;
 
 template<typename T>
-void show(const T &list);
-
-int main() {
-    list<string> lisNumbers = {"Danila", "Nikita", "Anton", "Stephan"};
-    cout << "Initial list:" << endl;
-    show(lisNumbers);
-
-    lisNumbers.reverse();
-    cout << "List after reversing:" << endl;
-    show(lisNumbers);
-
-    lisNumbers.sort();
-    cout << "List after sorting:" << endl;
-    show(lisNumbers);
-
-    return 0;
+void DisplayContents(const T &container) {
+    for (auto iElement = container.cbegin();
+         iElement != container.cend();
+         ++iElement)
+        cout << *iElement << endl;
+    cout << endl;
 }
 
-template<typename T>
-void show(const T &list) {
-    cout << "[ ";
-    for (auto iterator = list.cbegin(); iterator != list.cend(); ++iterator)
-        cout << *iterator << " ";
-    cout << "]" << endl;
+struct ContactItem {
+    string name;
+    string phoneNum;
+    string displayAs;
+
+    ContactItem(const string &nameInit, const string &phone) {
+        name = nameInit;
+        phoneNum = phone;
+        displayAs = (name + ": " + phoneNum);
+    }
+
+// used by set::find() given contact list item
+    bool operator==(const ContactItem &itemToCompare) const {
+        return (itemToCompare.phoneNum == this->phoneNum);
+    }
+
+// used to sort
+    bool operator<(const ContactItem &itemToCompare) const {
+        return (this->phoneNum < itemToCompare.phoneNum);
+    }
+
+// Used in DisplayContents via cout
+    operator const char *() const {
+        return displayAs.c_str();
+    }
+};
+
+int main() {
+    set<ContactItem> setContacts;
+    setContacts.insert(ContactItem("Jack Welsch", "+1 7889 879 879"));
+    setContacts.insert(ContactItem("Bill Gates", "+1 97 7897 8799 8"));
+    setContacts.insert(ContactItem("Angi Merkel", "+49 23456 5466"));
+    setContacts.insert(ContactItem("Vlad Putin", "+7 6645 4564 797"));
+    setContacts.insert(ContactItem("John Travolta", "91 234 4564 789"));
+    setContacts.insert(ContactItem("Ben Affleck", "+1 745 641 314"));
+    DisplayContents(setContacts);
+    cout << "Enter a number you wish to search: ";
+    string phoneNumber;
+    getline(cin, phoneNumber);
+    auto contactFound = setContacts.find(ContactItem("", phoneNumber));
+    if (contactFound != setContacts.end()) {
+        cout << "Contact " << phoneNumber << " belongs to " << contactFound->name << endl;
+    } else
+        cout << "Contact not found" << endl;
+    return 0;
 }
