e6689c8133a393a5112d8a803267cd5bf6356d5a
d9nich <dan.galaiko@gmail.com>
2020-06-30 12:16:12 +0300

    Exercise 20.2 has been done. The binary predicate fPredicate that helps the map sort a key of type WordProperty according to the string attribute it contains.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-template<typename T>
-void show(const T &elements) {
-    cout << "[";
-    for (auto start = elements.cbegin(); start != elements.cend(); ++start)
-        cout << *start << ", ";
-    cout << "]" << endl;
-}
+struct WordProperty {
+    string word;
+    bool isLatinBase;
+};
+
+class FPredicate {
+    bool operator()(const WordProperty &wp1, const WordProperty &wp2) const {
+        return wp1.word < wp2.word;
+    }
+};
 
 int main() {
-    unordered_multimap<string, string> telephoneDirectory;
     return 0;
 }
