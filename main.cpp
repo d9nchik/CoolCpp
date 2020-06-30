12acd37a9ac381704e395c8d7c1682b473df5812
d9nich <dan.galaiko@gmail.com>
2020-06-30 12:45:22 +0300

    Exercise 20.3 has been done. A simple program demonstartes, that a map cannot accept duplicate entries, whereas a multimap can.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,54 @@
-#include <unordered_map>
+#include <map>
 #include <iostream>
 #include <string>
-#include <utility>
 
 using namespace std;
 
-struct WordProperty {
-    string word;
-    bool isLatinBase;
-};
+template<typename T>
+void show_pair(const T &iterator) {
+    cout << "[" << iterator->first << ": " << iterator->second << "]";
+}
 
-class FPredicate {
-    bool operator()(const WordProperty &wp1, const WordProperty &wp2) const {
-        return wp1.word < wp2.word;
+template<typename T>
+void show_map(const T &map) {
+    cout << "[ ";
+    auto iterator = map.cbegin();
+    for (int i = 0; i < map.size() - 1; ++i) {
+        show_pair(iterator++);
+        cout << ",";
     }
-};
+
+    if (!map.empty())
+        show_pair(iterator);
+    cout << "]";
+}
 
 int main() {
+    map<string, int> myMap;
+    cout << "Testing map:" << endl;
+    myMap.insert(make_pair("Vova", 17));
+    myMap.insert(make_pair("Andriy", 4));
+    myMap.insert(make_pair("Natasha", 5));
+    cout << "Before adding duplicated value 'Andriy'" << endl;
+    show_map(myMap);
+    cout << endl;
+    myMap.insert(make_pair("Andriy", 7));
+    cout << "After adding duplicated value 'Andriy'" << endl;
+    show_map(myMap);
+    cout << endl;
+
+    multimap<string, int> myMultimap;
+    cout << "Testing map:" << endl;
+    myMultimap.insert(make_pair("Vova", 17));
+    myMultimap.insert(make_pair("Andriy", 4));
+    myMultimap.insert(make_pair("Natasha", 5));
+    cout << "Before adding duplicated value 'Andriy'" << endl;
+    show_map(myMultimap);
+    cout << endl;
+    myMultimap.insert(make_pair("Andriy", 7));
+    cout << "After adding duplicated value 'Andriy'" << endl;
+    show_map(myMultimap);
+    cout << endl;
+
     return 0;
 }
