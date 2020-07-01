2649ebade0de3f982839e082271525f3391b414a
d9nich <dan.galaiko@gmail.com>
2020-07-01 09:59:21 +0300

    Exercise 21.2 has been done. Extends this predicate to indicate the number of times it was used.

--- main.cpp
+++ main.cpp
@@ -5,17 +5,25 @@
 using namespace std;
 
 template<typename T>
-class showData {
+class ShowData {
+private:
+    unsigned int counter = 0;
 public:
     void operator()(const T &element) {
         cout << element << ", ";
+        ++counter;
+    }
+
+    unsigned int getCounter() const {
+        return counter;
     }
 
 };
 
 int main() {
-    vector<double> doubles ={3.2, 5.1, 4.7, 8.2, 2.0};
-    for_each(doubles.cbegin(), doubles.cend(), showData<double>());
-
+    vector<double> doubles = {3.2, 5.1, 4.7, 8.2, 2.0};
+    ShowData<double> showData = for_each(doubles.cbegin(), doubles.cend(), ShowData<double>());
+    cout << endl;
+    cout << "ShowData() has been called " << showData.getCounter() << " times.";
     return 0;
 }
